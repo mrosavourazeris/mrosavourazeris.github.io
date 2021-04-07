163632cbefb7edf0ca8850c9c18962113a2f81f9
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-04-07 07:15:06 -0500

    Picked style for portfolio card. Now just need to turn it into a component.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -13,61 +13,24 @@ const Portfolio = () => (
   <div className="container">
       <section>
         <div id="myCard" className="card bg-light mb-5 ml-5 mr-5">
-          <div className="card-header bg-dark text-light">
-            <h1>Portfolio</h1>
+          <div className="card-header bg-dark text-light" >
+            <h1 style={{textAlign: "center"}}>Password-Generator</h1>
           </div>
           <div id="myCardBody" className="card-body">
-            <div id="myRow" className="row" style={{textAlign: "center"}}>
-                <h3 className="portfolioTitle" style={{marginBottom: "45px", fontSize: "50px"}}>Password-Generator</h3>
-            </div> 
             <div id="myRow" className="row ">
               <div className="col">
                 <img className="img-fluid mb-2 mr-3 float-left imageBorder portfolioPics" style={{border: "solid"}} src={myPassword} alt="Picture of Michael Rosa-Vourazeris" className="card-img-top"></img>
               </div>
               <div className="col">
+                <h2 style={{textDecoration: "underline"}}>Description</h2>
                 <p><a href={"https://github.com/mrosavourazeris/Password-Generator"}>GitHub Repository</a> Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h2 style={{textDecoration: "underline"}}>Links</h2>
+                <p class="myPTag"><a href="https://mrosavourazeris.github.io/Password-Generator/">Deployed Version</a></p>
+                <p><a href="https://github.com/mrosavourazeris/Password-Generator">GitHub Repository</a></p>
               </div>
             </div> 
           </div>
-          <div id="myCardBody" className="card-body">
-            <div id="myRow" className="row" style={{textAlign: "center"}}>
-                <h3 className="portfolioTitle" style={{marginBottom: "45px", fontSize: "50px"}}>Password-Generator</h3>
-            </div> 
-            <div id="myRow" className="row ">
-              <div className="col">
-                <img className="img-fluid mb-2 mr-3 float-left imageBorder portfolioPics" style={{border: "solid"}} src={myPassword} alt="Picture of Michael Rosa-Vourazeris" className="card-img-top"></img>
-              </div>
-              <div className="col" style={{textAlign: 'center', fontSize: "30px"}}>
-                <div className="container justify-content-center">
-                  <p className="myPTag"><a href={"https://mrosavourazeris.github.io/Password-Generator/"}>Deployed Version</a></p>
-                  <p><a href={"https://github.com/mrosavourazeris/Password-Generator"}>GitHub Repository</a></p>
-                </div>
-              </div>
-              <div className="col">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </div>
-            </div> 
-          </div>
-          <div id="myCardBody" className="card-body">
-            <div id="myRow" className="row" style={{textAlign: "center"}}>
-                <h3 className="portfolioTitle" style={{marginBottom: "45px", fontSize: "50px"}}>Password-Generator</h3>
-            </div> 
-            <div id="myRow" className="row ">
-              <div className="col">
-                <img className="img-fluid mb-2 mr-3 float-left imageBorder portfolioPics" style={{border: "solid"}} src={myPassword} alt="Picture of Michael Rosa-Vourazeris" className="card-img-top"></img>
-              </div>
-              <div className="col" style={{textAlign: 'center', fontSize: "30px"}}>
-                <div className="container justify-content-center">
-                  <p className="myPTag"><a href={"https://mrosavourazeris.github.io/Password-Generator/"}>Deployed Version</a></p>
-                  <p><a href={"https://github.com/mrosavourazeris/Password-Generator"}>GitHub Repository</a></p>
-                </div>
-              </div>
-              <div className="col">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </div>
-            </div> 
-          </div>
-        </div>
+        </div>        
       </section>
     </div>
 )
